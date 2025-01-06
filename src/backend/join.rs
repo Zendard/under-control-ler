b1004a8ea356ed1054d235bf05ea56b2dbb63cf8
Zendard <thijsmuylaert@hotmail.com>
2025-01-06 21:57:40 +0100

    Let client wait until they are accepted

--- src/backend/join.rs
+++ src/backend/join.rs
@@ -1,6 +1,5 @@
 use super::{open_socket, NetworkMessage, NetworkMessageSender};
 use crate::{BackendMessage, FrontendMessage};
-use core::net;
 use iced::futures::{
     channel::mpsc::{Receiver, Sender},
     executor::block_on,
@@ -23,6 +22,22 @@ pub fn join(
     network_sender
         .send_network_message(NetworkMessage::JoinRequest)
         .unwrap();
+
+    // Wait until we are accepted
+    loop {
+        // Check for new network requests
+        let received_data = network_sender.socket.next_message();
+
+        // Only continue when received data is not an error and a ClientAccepted message
+        if let Some((NetworkMessage::ClientAccepted, origin)) = received_data {
+            dbg!(&received_data);
+            // Only break when origin is the host we want to connect to
+            if origin == network_sender.destination {
+                break;
+            }
+        }
+    }
+    println!("We were accepted");
 }
 
 fn ping(socket: NetworkMessageSender, socket_addr: SocketAddr, mut sender: Sender<BackendMessage>) {
