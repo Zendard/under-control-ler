501f67d0d29e234b09ef1d6c6c034b56a236e2ad
Zendard <thijsmuylaert@hotmail.com>
2025-01-23 20:48:31 +0100

    Save x and y for thumbsticks on windows so it doesn't bug out as musch

--- src/backend/hosting/windows.rs
+++ src/backend/hosting/windows.rs
@@ -9,14 +9,26 @@ use windows::{
     UI::Input::Preview::Injection::{InjectedInputGamepadInfo, InputInjector},
 };
 
-pub struct VirtualGamepad(InputInjector);
+pub struct VirtualGamepad {
+    injector: InputInjector,
+    left_x: f64,
+    left_y: f64,
+    right_x: f64,
+    right_y: f64,
+}
 unsafe impl Send for VirtualGamepad {}
 
 impl VirtualGamepad {
     pub fn new() -> Result<Self, Box<dyn Error>> {
         let injector = InputInjector::TryCreate()?;
         injector.InitializeGamepadInjection()?;
-        Ok(Self(injector))
+        Ok(Self {
+            injector,
+            left_x: 0 as f64,
+            left_y: 0 as f64,
+            right_x: 0 as f64,
+            right_y: 0 as f64,
+        })
     }
 
     pub fn play_input(&mut self, input: GamepadInput) {
@@ -30,7 +42,7 @@ impl VirtualGamepad {
             let button = GamepadButtons::None;
             let injected_input = InjectedInputGamepadInfo::new().unwrap();
             injected_input.SetButtons(button).ok();
-            self.0.InjectGamepadInput(&injected_input).unwrap();
+            self.injector.InjectGamepadInput(&injected_input).unwrap();
             return;
         }
 
@@ -52,7 +64,28 @@ impl VirtualGamepad {
         };
         let injected_input = InjectedInputGamepadInfo::new().unwrap();
         injected_input.SetButtons(button).ok();
-        self.0.InjectGamepadInput(&injected_input).unwrap();
+        self.injector.InjectGamepadInput(&injected_input).unwrap();
     }
-    fn change_axis(&mut self, axis: AxisInput, value: i8) {}
+    fn change_axis(&mut self, axis: AxisInput, value: i8) {
+        let injected_input = InjectedInputGamepadInfo::new().unwrap();
+
+        let value: f64 = value as f64 / 127 as f64;
+
+        match axis {
+            AxisInput::StickLeftX => self.left_x = value,
+            AxisInput::StickLeftY => self.left_y = value,
+            AxisInput::StickRightX => self.right_x = value,
+            AxisInput::StickRightY => self.right_y = value,
+            AxisInput::TriggerLeft => injected_input.SetLeftTrigger(value).unwrap(),
+            AxisInput::TriggerRight => injected_input.SetRightTrigger(value).unwrap(),
+        };
+
+        injected_input.SetLeftThumbstickX(self.left_x).unwrap();
+        injected_input.SetLeftThumbstickY(self.left_y).unwrap();
+
+        injected_input.SetRightThumbstickX(self.right_x).unwrap();
+        injected_input.SetRightThumbstickY(self.right_y).unwrap();
+
+        self.injector.InjectGamepadInput(&injected_input).unwrap();
+    }
 }
